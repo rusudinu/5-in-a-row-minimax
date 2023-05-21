34816ed67e848cc99dc468cfa22ad13eacc5ddbb
Rusu Dinu <dinu@codingshadows.com>
2023-05-21 10:25:17 +0300

    wrong minimax v2

--- src/main/scala/AI.scala
+++ src/main/scala/AI.scala
@@ -1,68 +1,40 @@
-import BoardUtils.{Board, complement, display, next, scoreBoard, winner}
+import BoardUtils.{Board, complement, next, scoreBoard, winner}
 import Trace.time
 
 object AI {
 
-  def minimax(p: Player)(b: Board): Board = time("minimax") {
-    def aux(shouldMaximize: Boolean)(b: Board): Int = {
-      if (winner(p)(b)) 1
-      else if (winner(complement(p))(b)) -1
-      else if (next(p)(b).isEmpty) 0
-      else {
-        val scores = for (nextBoard <- next(p)(b)) yield aux(!shouldMaximize)(nextBoard)
-        if (shouldMaximize) scores.max else scores.min
+  trait MinimaxTree
+
+  case class MinimaxNode(score: Int, board: Board, children: List[MinimaxTree]) extends MinimaxTree
+
+  case class MinimaxLeaf(score: Int, board: Board) extends MinimaxTree
+
+  def minimax(p: Player)(b: Board): MinimaxTree = time("minimax") {
+    def minimaxHelper(p: Player)(b: Board, depth: Int): MinimaxTree = {
+      if (winner(p)(b) || winner(complement(p))(b) || depth == 0) {
+        MinimaxLeaf(scoreBoard(p)(b), b)
+      } else {
+        val children = next(p)(b).map(minimaxHelper(complement(p))(_, depth - 1))
+        MinimaxNode(scoreBoard(p)(b), b, children)
       }
     }
 
-    next(p)(b).maxBy(aux(shouldMaximize = false))
+    minimaxHelper(p)(b, 3)
   }
-
-  // create a history map, with the board as key and the score as value
-
-  var historyMap: Map[Board, Int] = Map()
-
-  def minimaxWithHistoryMapAndMaxDepth(p: Player)(b: Board): Board = time("minimax-with-history") {
-    def aux(shouldMaximize: Boolean, depth: Int)(b: Board): Int = {
-      if (winner(p)(b)) 1
-      else if (winner(complement(p))(b)) -1
-      else if (next(p)(b).isEmpty) 0
-      else if (depth == 0) scoreBoard(p)(b)
-      else {
-        val scores = for (nextBoard <- next(p)(b))
-          yield {
-            if (historyMap.contains(nextBoard)) historyMap(nextBoard)
-            else {
-              val score = aux(!shouldMaximize, depth - 1)(nextBoard)
-              historyMap += (nextBoard -> score)
-              score
-            }
-          }
-        if (shouldMaximize) scores.max else scores.min
-      }
-    }
-
-    next(p)(b).maxBy(aux(shouldMaximize = false, 8))
-  }
-
-  def minimaxWithDepth(p: Player)(b: Board): Board = time("minimax2") {
-    def aux(shouldMaximize: Boolean, depth: Int)(b: Board): Int = {
-      if (winner(p)(b)) 1
-      else if (winner(complement(p))(b)) -1
-      else if (next(p)(b).isEmpty) 0
-      else if (depth == 0) scoreBoard(p)(b)
-      else {
-        val scores = for (nextBoard <- next(p)(b)) yield aux(!shouldMaximize, depth - 1)(nextBoard)
-        if (shouldMaximize) scores.max else scores.min
-      }
-    }
-
-    next(p)(b).maxBy(aux(shouldMaximize = false, 4))
-  }
-
-  // try and make minimax that keeps in mind the previous board scores such that they can be reused in the next call of minimax
 
   def predictNextBestMove(p: Player)(b: Board): Board = time("predict-next-best-move") {
-    // minimaxWithDepth(p)(b) // 23866 ms average, 4 depth
-    minimaxWithHistoryMapAndMaxDepth(p)(b) // 519 ms average, 4 depth | 15984 ms average, 8 depth
+    val tree = minimax(p)(b)
+    tree match {
+      case MinimaxNode(_, _, children) =>
+        val bestChild = children.maxBy {
+          case MinimaxLeaf(score, _) => score
+          case MinimaxNode(score, _, _) => score
+        }
+        bestChild match {
+          case MinimaxLeaf(_, board) => board
+          case MinimaxNode(_, board, _) => board
+        }
+      case MinimaxLeaf(_, board) => board
+    }
   }
 }
