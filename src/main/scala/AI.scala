a073566cd7f25a0f171768c7433d17d685858f84
Rusu Dinu <dinu@codingshadows.com>
2023-05-21 10:17:50 +0300

    wrong minimax

--- src/main/scala/AI.scala
+++ src/main/scala/AI.scala
@@ -1,12 +1,68 @@
-import BoardUtils.{Board, display, next, scoreBoard}
+import BoardUtils.{Board, complement, display, next, scoreBoard, winner}
 import Trace.time
 
 object AI {
-  // using minimax algorithm
+
+  def minimax(p: Player)(b: Board): Board = time("minimax") {
+    def aux(shouldMaximize: Boolean)(b: Board): Int = {
+      if (winner(p)(b)) 1
+      else if (winner(complement(p))(b)) -1
+      else if (next(p)(b).isEmpty) 0
+      else {
+        val scores = for (nextBoard <- next(p)(b)) yield aux(!shouldMaximize)(nextBoard)
+        if (shouldMaximize) scores.max else scores.min
+      }
+    }
+
+    next(p)(b).maxBy(aux(shouldMaximize = false))
+  }
+
+  // create a history map, with the board as key and the score as value
+
+  var historyMap: Map[Board, Int] = Map()
+
+  def minimaxWithHistoryMapAndMaxDepth(p: Player)(b: Board): Board = time("minimax-with-history") {
+    def aux(shouldMaximize: Boolean, depth: Int)(b: Board): Int = {
+      if (winner(p)(b)) 1
+      else if (winner(complement(p))(b)) -1
+      else if (next(p)(b).isEmpty) 0
+      else if (depth == 0) scoreBoard(p)(b)
+      else {
+        val scores = for (nextBoard <- next(p)(b))
+          yield {
+            if (historyMap.contains(nextBoard)) historyMap(nextBoard)
+            else {
+              val score = aux(!shouldMaximize, depth - 1)(nextBoard)
+              historyMap += (nextBoard -> score)
+              score
+            }
+          }
+        if (shouldMaximize) scores.max else scores.min
+      }
+    }
+
+    next(p)(b).maxBy(aux(shouldMaximize = false, 8))
+  }
+
+  def minimaxWithDepth(p: Player)(b: Board): Board = time("minimax2") {
+    def aux(shouldMaximize: Boolean, depth: Int)(b: Board): Int = {
+      if (winner(p)(b)) 1
+      else if (winner(complement(p))(b)) -1
+      else if (next(p)(b).isEmpty) 0
+      else if (depth == 0) scoreBoard(p)(b)
+      else {
+        val scores = for (nextBoard <- next(p)(b)) yield aux(!shouldMaximize, depth - 1)(nextBoard)
+        if (shouldMaximize) scores.max else scores.min
+      }
+    }
+
+    next(p)(b).maxBy(aux(shouldMaximize = false, 4))
+  }
+
+  // try and make minimax that keeps in mind the previous board scores such that they can be reused in the next call of minimax
+
   def predictNextBestMove(p: Player)(b: Board): Board = time("predict-next-best-move") {
-    val nextMoves = next(p)(b)
-    val scores = nextMoves.map(scoreBoard(p))
-    println(s"Scores: $scores")
-    nextMoves.maxBy(scoreBoard(p))
+    // minimaxWithDepth(p)(b) // 23866 ms average, 4 depth
+    minimaxWithHistoryMapAndMaxDepth(p)(b) // 519 ms average, 4 depth | 15984 ms average, 8 depth
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,55 +1,7 @@
-import BoardUtils.{makeBoard, scoreBoard, sequences}
 import Game.start
 
 object Main {
   def main(args: Array[String]): Unit = {
-    /*
-    println(scoreLine(One)(List(One, One, One, One, One)))
-    println(scoreLine(One)(List(One, One, One, Empty, Empty)))
-    println(scoreLine(One)(List(One, Empty, One, One, One, One)))
-     */
-    val test =
-      """00000
-        |0000X
-        |000..
-        |00.0.
-        |0X..0""".stripMargin.replace("\r\n", "\n")
-    val test2 =
-      """00X00
-        |0000X
-        |0X0..
-        |00X0.
-        |0X..0""".stripMargin.replace("\r\n", "\n")
-    val test3 =
-      """00X00
-        |0000X
-        |0X0..
-        |00X0.
-        |XX..X""".stripMargin.replace("\r\n", "\n")
-    val test4 =
-      """00X00
-        |0000X
-        |0X0..
-        |00X0.
-        |XXXX.""".stripMargin.replace("\r\n", "\n")
-    val test5 =
-      """00X00
-        |0000X
-        |0X0..
-        |00X0.
-        |XXX.X""".stripMargin.replace("\r\n", "\n")
-    val test6 =
-      """00X00
-        |0000X
-        |0X0..
-        |00X0.
-        |XX0.X""".stripMargin.replace("\r\n", "\n")
-    println(scoreBoard(One)(makeBoard(test)))
-    println(scoreBoard(One)(makeBoard(test2)))
-    println(scoreBoard(One)(makeBoard(test3)))
-    println(scoreBoard(One)(makeBoard(test4)))
-    println(scoreBoard(One)(makeBoard(test5)))
-    println(scoreBoard(One)(makeBoard(test6)))
     start()
   }
 }
