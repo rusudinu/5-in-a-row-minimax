254ccfc04264a92d7c75dbcf48c9455e8617dbc3
Rusu Dinu <dinu@codingshadows.com>
2023-05-27 15:30:38 +0300

    memoizing score-board

--- src/main/scala/AI.scala
+++ src/main/scala/AI.scala
@@ -1,5 +1,6 @@
 import BoardUtils.{Board, complement, next, playedMoves, scoreBoard, winner}
 import Trace.time
+import scalaz.Memo
 
 object AI {
 
@@ -15,7 +16,7 @@ object AI {
 
   private def minimax(p: Player)(b: Board, depth: Int, maximizing: Boolean, alpha: Int, beta: Int): Int = time("minimax") {
     if (depth == 0 || winner(p)(b) || alpha >= beta) {
-      scoreBoardWrapper(p)(b)(maximizing)
+      memoizedScoreBoard(p, b, maximizing)
     } else {
       next(p)(b).foldLeft(alpha, beta, if (maximizing) Int.MinValue else Int.MaxValue)(
         (acc, child) => {
@@ -36,6 +37,9 @@ object AI {
     2
   }
 
+  private val memoizedScoreBoard = Memo.mutableHashMapMemo[(Player, Board, Boolean), Int] {
+    case (p, b, maximizing) => scoreBoardWrapper(p)(b)(maximizing)
+  }
 
   def predictNextBestMove(p: Player)(b: Board): Board = time("predict-next-best-move") {
     next(p)(b).maxBy(brd => minimax(complement(p))(brd, depth(brd), maximizing = false, Int.MinValue, Int.MaxValue))
